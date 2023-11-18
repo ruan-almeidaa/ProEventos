6ce513ae33e86a3504c42ed8e747fd596598bb7c
Ruan <ruanalmeidawegner@gmail.com>
2023-11-18 16:01:05 -0300

    inicializando repositório

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular",
+                    Local = "Igrejinha/RS",
+                    Lote = "1",
+                    QtdPessoas = 100,
+                    DataEvento = DateTime.Now.AddDays(3).ToString(),
+                    ImagemURL = "foto.jpeg"
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Node",
+                    Local = "Igrejinha/RS",
+                    Lote = "4",
+                    QtdPessoas = 1200,
+                    DataEvento = DateTime.Now.AddDays(6).ToString(),
+                    ImagemURL = "fotoEv2.jpeg"
+                }
+
+        };
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetbyId(int id)
+    {
+        return _evento.Where(evento => evento.EventoId == id);
+    }
+}
+}
